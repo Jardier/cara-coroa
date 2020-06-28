613f18e7edf6b60fa72f8674b456498cc987a050
Jardier <jardier@hotmail.com>
2020-06-28 17:04:19 -0300

    Finalizando aplicação

--- app/src/main/java/com/android/cara_coroa/MainActivity.kt
+++ app/src/main/java/com/android/cara_coroa/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.android.cara_coroa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btJogar : Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btJogar =  findViewById(R.id.btJogar);
+
+        //Action
+        btJogar.setOnClickListener{
+
+            val intent = Intent(applicationContext, ResultadoActivity::class.java);
+
+            val numero : Int = Random.nextInt(2);
+            intent.putExtra("NUMERO", numero);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/android/cara_coroa/ResultadoActivity.kt
+++ app/src/main/java/com/android/cara_coroa/ResultadoActivity.kt
@@ -2,11 +2,40 @@ package com.android.cara_coroa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class ResultadoActivity : AppCompatActivity() {
+
+    lateinit var imagemMoeda : ImageView;
+    lateinit var btVoltar     : Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        imagemMoeda = findViewById(R.id.ivMoeda);
+        btVoltar = findViewById(R.id.btVoltar);
+
+        val numero  = intent.getIntExtra("NUMERO", -1);
+
+        when(numero){
+            0 -> imagemMoeda.setImageResource(R.drawable.moeda_cara);
+            1 -> imagemMoeda.setImageResource(R.drawable.moeda_coroa);
+            else -> {
+                imagemMoeda.setImageResource(R.drawable.moeda_sem_cara);
+            }
+
+        }
+
+        //Configurando botão voltar
+        btVoltar.setOnClickListener({
+            finish();
+        })
     }
+
+
 }
+
+

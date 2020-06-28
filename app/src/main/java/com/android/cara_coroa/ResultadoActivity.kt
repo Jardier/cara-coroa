e1d1ba456ea7e8fa2769dd3399ad4e685d4d864a
Jardier <jardier@hotmail.com>
2020-06-28 15:47:54 -0300

    Criando a interface do Jogo

---
+++ app/src/main/java/com/android/cara_coroa/ResultadoActivity.kt
@@ -0,0 +1,12 @@
+package com.android.cara_coroa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
